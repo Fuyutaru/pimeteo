b02fd844c411423778199ef7aed7020bbb5cfd6c
vpech77 <vanessa.pech77@gmail.com>
2025-02-04 17:09:42 +0100

    add map

---
+++ dashboard-vue/src/components/HeaderApp.vue
@@ -0,0 +1,35 @@
+<template>
+        <nav>
+        <ul>
+            <li>Logo</li>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Services</li>
+        </ul>
+        <form>
+            <input type="text">
+            <button>Go</button>
+        </form>
+
+    </nav>
+</template>
+
+<style>
+nav {
+    background: lightsteelblue;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+ul {
+    padding: 0;
+    display: flex;
+}
+li {
+    margin: 10px;
+    list-style-type: none;
+}
+form {
+    margin-right: 10px;
+}
+</style>
---
+++ dashboard-vue/src/components/Map.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        var map = new maplibregl.Map({
+        container: 'map',
+        style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9 // starting zoom
+        });
+    }
+}
+
+
+</script>
+
+
+<style scoped>
+#map {
+  height: 100vh;
+  width: 100vh;
+}
+</style>
