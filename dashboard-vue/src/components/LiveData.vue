b27670124d4c89825b90aa622c0c6ce04807d37a
vpech77 <vanessa.pech77@gmail.com>
2025-02-05 12:11:14 +0100

    correct fetch live

--- dashboard-vue/src/components/LiveData.vue
+++ dashboard-vue/src/components/LiveData.vue
@@ -6,17 +6,20 @@ export default {
         dataLive: {},
     }
   },
+  mounted(){
+    fetch("./live.json")
+        .then(response => response.json())
+        .then(json => this.dataLive=json);
+  },
+  
   methods: {
     hello(){
-        console.log("hello")
+        console.log(this.dataLive.data.date);
     },
     read(){
-        fetch("./quiz.json")
-        .then(response => response.json())
-        .then(json => console.log(json));
-        }
+
     }
-}
+}}
 
 
 </script>
@@ -25,7 +28,7 @@ export default {
     <!-- <h1>Comment récupérer une data qui est dans un autre component</h1>
     <h1>Comment load un json local file</h1> -->
     <ul class="list-group">
-        <li class="list-group-item" @click="read">An item</li>
+        <li class="list-group-item" @click="hello">An item</li>
         <li class="list-group-item">A second item</li>
         <li class="list-group-item">A third item</li>
         <li class="list-group-item">A fourth item</li>
