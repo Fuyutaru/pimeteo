5a1df80023421e984901181cd6ecb9335dcb1267
Fuyutaru <zijian1105@gmail.com>
2025-02-06 15:57:19 +0100

    true token

--- meteoApp/script.js
+++ meteoApp/script.js
@@ -3,20 +3,20 @@ const chokidar = require('chokidar');
 // import fs from 'fs';
 const fs = require('fs');
 // import {InfluxDB, Point} from '@influxdata/influxdb-client';
-const {InfluxDB, Point} = require('@influxdata/influxdb-client');
+const { InfluxDB, Point } = require('@influxdata/influxdb-client');
 // import nmea from 'node-nmea';
 // import nmea from 'nmea-simple'
 const nmea = require('nmea-simple')
 
-let temp_data ={}
+let temp_data = {}
 
 let rain_value;
 
 let interval = 30000;
 
 const watcher = chokidar.watch('/dev/shm/sensors', {
-    persistent: true,
-  });
+  persistent: true,
+});
 
 const tpg_watcher = chokidar.watch('/dev/shm/tpg.log', {
   persistent: true,
@@ -33,7 +33,8 @@ const gps_watcher = chokidar.watch('/dev/shm/gpsNmea', {
 function addData() {
   if (!(temp_data.length === 0)) {
     // raspi
-    const token = 'wn_EZ-uLnCtL68y6T-d8pshTw-S7bAa7mNnyAWIkvWT8OgEIuegAS5xGSKakJbjLEmsVGzrY0wxUFWdbB4lzMA==';
+    // const token = 'F8bh5nAMrb7zo43oTtPIvZxES2EtdceLvJ4lWld4k9Se10047DgpMitlNhEw2PkHtkjjDLxY-MVrhsTpK5jLDA==';
+    const token = process.env.INFLUXDB_TOKEN
     // zijian
     // const token = 'sf70vN5suVwlorMq1IBkAmzMLb7Bu4OPOxT4oDFwVCw3GvgsTTrkQQ_SgjRMesQSIxBtqk5sFnf5e_jIdtp1Mg==';
     const url = 'http://localhost:8086';
@@ -65,55 +66,55 @@ function addData() {
 
 // watcher.add(['/dev/shm/sensors', '/dev/shm/rainCounter.log', '/dev/shm/gpsNmea', '/dev/shm/tpg.log'])
 
-watcher.on('change', ()=>{
+watcher.on('change', () => {
 
-    fs.readFile('/dev/shm/sensors', (err, data) => {
-        if (err) {
-          console.error('Problème de lecture:', err);
-          return;
-        }
+  fs.readFile('/dev/shm/sensors', (err, data) => {
+    if (err) {
+      console.error('Problème de lecture:', err);
+      return;
+    }
 
 
-      
-        try {
-          const jsonData = JSON.parse(data);
-          jsonData.measure.forEach(element => {
-            if (element["name"] != "temperature" || element["name"] != "pressure"){
-              temp_data[element["name"]] = element["value"];
-            }
-          });
-        
-          }
-       
-        catch (err) {
-          console.error('Error parsing JSON:', err);
+
+    try {
+      const jsonData = JSON.parse(data);
+      jsonData.measure.forEach(element => {
+        if (element["name"] != "temperature" || element["name"] != "pressure") {
+          temp_data[element["name"]] = element["value"];
         }
       });
+
+    }
+
+    catch (err) {
+      console.error('Error parsing JSON:', err);
+    }
+  });
 
 
 })
 
 
-tpg_watcher.on('change', ()=>{
+tpg_watcher.on('change', () => {
 
   fs.readFile('/dev/shm/tpg.log', (err, data) => {
     if (err) {
       console.error('Problème de lecture:', err);
       return;
     }
-  
+
     try {
       const jsonData = JSON.parse(data);
       console.log(jsonData);
 
-      for (const [key, value] of Object.entries(jsonData)){
-        if (key != "date" || key != "hygro"){
+      for (const [key, value] of Object.entries(jsonData)) {
+        if (key != "date" || key != "hygro") {
           temp_data[key] = value;
 
           // const point = new Point(key)
           //   .floatField('value', value)
 
-          
+
           // void setTimeout(() => {
           //   writeClient.writePoint(point)
           // }, 1000) // separate points by 1 second
@@ -124,9 +125,9 @@ tpg_watcher.on('change', ()=>{
 
         }
       }
-    
-      }
-    
+
+    }
+
     catch (err) {
       console.error('Error parsing JSON:', err);
     }
@@ -135,7 +136,7 @@ tpg_watcher.on('change', ()=>{
 })
 
 
-rain_watcher.on('change', ()=>{
+rain_watcher.on('change', () => {
 
 
   fs.readFile('/dev/shm/rainCounter.log', 'utf8', (err, data) => {
@@ -143,24 +144,24 @@ rain_watcher.on('change', ()=>{
       console.error('Problème de lecture:', err);
       return;
     }
-  
+
     try {
       console.log(data);
-      if (!("rain" in temp_data)){
+      if (!("rain" in temp_data)) {
         temp_data["rain"] = 0.328;
         rain_value = data;
       }
-      else{
-        if(rain_value != data){
+      else {
+        if (rain_value != data) {
           temp_data["rain"] += 0.328;
           rain_value = data;
         }
       }
 
       console.log(temp_data)
-    
+
     }
-    
+
     catch (err) {
       console.error('Error parsing JSON:', err);
     }
@@ -169,36 +170,36 @@ rain_watcher.on('change', ()=>{
 
 })
 
-gps_watcher.on('change', ()=>{
+gps_watcher.on('change', () => {
 
   fs.readFile('/dev/shm/gpsNmea', 'utf8', (err, data) => {
     if (err) {
       console.error('Problème de lecture:', err);
       return;
     }
-  
+
 
     try {
       let raw = data.split("\n")[1];
       const parsed = nmea.parseNmeaSentence(raw);
 
       if (parsed.latitude !== undefined && parsed.longitude !== undefined) {
-          console.log(`Latitude (DD): ${parsed.latitude.toFixed(3)}`);
-          console.log(`Longitude (DD): ${parsed.longitude.toFixed(3)}`);
-          let lat = parsed.latitude.toFixed(3);
-          let lon = parsed.longitude.toFixed(3);
+        console.log(`Latitude (DD): ${parsed.latitude.toFixed(3)}`);
+        console.log(`Longitude (DD): ${parsed.longitude.toFixed(3)}`);
+        let lat = parsed.latitude.toFixed(3);
+        let lon = parsed.longitude.toFixed(3);
 
-          temp_data["lat"] = lat;
-          temp_data["lon"] = lon;
+        temp_data["lat"] = lat;
+        temp_data["lon"] = lon;
 
-          console.log(temp_data)
+        console.log(temp_data)
 
 
       } else {
-          console.log("No latitude/longitude in this sentence.");
+        console.log("No latitude/longitude in this sentence.");
       }
-    } 
-    
+    }
+
     catch (err) {
       console.error('Error parsing JSON:', err);
     }
