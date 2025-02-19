7db8ea08b28f5f2364bc8fb96bad65ea4d8d7a63
Vpech77 <vanessa.pech77@gmail.com>
2025-02-19 14:15:59 +0100

    bouton header cliqued

--- dashboard/src/components/HeaderApp.vue
+++ dashboard/src/components/HeaderApp.vue
@@ -1,5 +1,3 @@
-<script setup></script>
-
 <template>
   <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
     <a
@@ -10,13 +8,42 @@
     </a>
 
     <ul class="nav nav-pills me-5">
-      <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Pi 28</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Pi 27</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Pi 30</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Pi 31</a></li>
-      <li class="nav-item"><a href="#" class="nav-link">Pi 32</a></li>
+      <li class="nav-item">
+          <a href="#" class="nav-link" :class="{ active: stationName === 'Pi 28' }" @click="stationCliqued('Pi 28')">Pi 28</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link" :class="{ active: stationName === 'Pi 27' }" @click="stationCliqued('Pi 27')">Pi 27</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link" :class="{ active: stationName === 'Pi 30' }" @click="stationCliqued('Pi 30')">Pi 30</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link" :class="{ active: stationName === 'Pi 31' }" @click="stationCliqued('Pi 31')">Pi 31</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link" :class="{ active: stationName === 'Pi 32' }" @click="stationCliqued('Pi 32')">Pi 32</a>
+        </li>
     </ul>
   </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      stationName: "Pi 28",
+    }
+  },
+  methods: {
+    stationCliqued(newStationName) {
+      this.stationName = newStationName;
+      console.log(this.stationName);
+    }
+  }
+
+
+}
+
+</script>
 
 <style scoped></style>
