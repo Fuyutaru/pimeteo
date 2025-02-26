87a96c68e32fed2f8fbb431ff92adfc1297d2bcf
vpech77 <vanessa.pech77@gmail.com>
2025-02-26 16:05:09 +0100

    add mapview

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,7 +4,8 @@ import MapView from './views/MapView.vue'
 </script>
 
 <template>
-  <MapView />
+  <RouterView />
+  <router-link to="/map" class="btn btn-outline-danger btn-lg m-4" data-bs-toggle="button">Go MAP</router-link>
 </template>
 
 <style scoped></style>
