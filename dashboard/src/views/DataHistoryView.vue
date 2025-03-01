b0bfb3e64258ec708398f59f0b2183073421a4f1
Vpech77 <vanessa.pech77@gmail.com>
2025-03-01 18:40:04 +0100

    add composable icon

--- dashboard/src/views/DataHistoryView.vue
+++ dashboard/src/views/DataHistoryView.vue
@@ -1,45 +1,38 @@
 <template>
   <div class="container">
-      <div class="row mb-4">
-          <div class="col">
-              <HeaderApp @updateStationName="maj_station" />
-              <InfoStation :stationName="stationName" :timestamp="timestamp"/>
-          </div>
+    <div class="row mb-4">
+      <div class="col">
+        <HeaderApp @updateStationName="maj_station" />
+        <InfoStation :stationName="stationName" :timestamp="timestamp" />
       </div>
-      <div class="row">
-          <div class="col-2">
-              <MenuApp @updateSensor="maj_sensor" />
-              <div v-if="sensorList.length !== 0">
-                <MenuDate @updateTimeRange="maj_timeRange"/>
-              </div>
-              <div v-else>
-                <div class="alert alert-warning d-flex align-items-center mt-4" role="alert">
-                    Please select at least one sensor
-                </div>
-              </div>
-              
+    </div>
+    <div class="row">
+      <div class="col-2">
+        <MenuApp @updateSensor="maj_sensor" />
+        <div v-if="sensorList.length !== 0">
+          <MenuDate @updateTimeRange="maj_timeRange" />
+        </div>
+        <div v-else>
+          <div class="alert alert-warning d-flex align-items-center mt-4" role="alert">
+            Please select at least one sensor
           </div>
-          <div class="col-10">
-            <DataHistoryBoard :sensorData="sensorData" :location="location"/>
-          </div>
+        </div>
       </div>
+      <div class="col-10">
+        <DataHistoryBoard :sensorData="sensorData" :location="location" />
+      </div>
+    </div>
   </div>
 </template>
-  
+
 <script>
 import HeaderApp from '@/components/HeaderApp.vue'
-import MenuApp from '@/components/MenuApp.vue'
-import DataHistoryBoard from '@/components/DataHistoryBoard.vue'
-import TemperatureIcon from '@/assets/temperature.png'
 import InfoStation from '@/components/InfoStation.vue'
-import LumIcon from '@/assets/luminosity.png'
-import HumIcon from '@/assets/humidity.png'
-import PrecipIcon from '@/assets/precipitation.png'
-import HeadIcon from '@/assets/wind_head.png'
-import SpeedIcon from '@/assets/wind_speed.png'
-import PressIcon from '@/assets/pressure.png'
+import MenuApp from '@/components/MenuApp.vue'
 import MenuDate from '@/components/MenuDate.vue'
-  
+import DataHistoryBoard from '@/components/DataHistoryBoard.vue'
+import { useSensorIcons } from '@/components/composables/iconSensor.js'
+
 export default {
   components: {
     HeaderApp,
@@ -53,114 +46,107 @@ export default {
       sensorList: [],
       sensorData: [],
       dataHistory: {},
-      timestamp: "",
-      timerange: {start: '', stop: ''},
-      location: {lon: 0, lat: 0},
-      stationName: "Pi 28",
-      sensorName: {"rain": "Precipitation", 
-                   "temperature": "Temperature", 
-                   "humidity": "Humidity",
-                   "pressure": "Pressure",
-                   "wind_speed_avg": "Wind Speed",
-                   "wind_heading": "Wind Heading",
-                   "luminosity": "Luminosity"
-                  },
-      sensorIcon: {"rain": PrecipIcon, 
-                   "temperature": TemperatureIcon, 
-                   "humidity": HumIcon,
-                   "pressure": PressIcon,
-                   "wind_speed_avg": SpeedIcon,
-                   "wind_heading": HeadIcon,
-                   "luminosity": LumIcon
-                  },
+      timestamp: '',
+      timerange: { start: '', stop: '' },
+      location: { lon: 0, lat: 0 },
+      stationName: 'Pi 28',
+      sensorName: {
+        rain: 'Precipitation',
+        temperature: 'Temperature',
+        humidity: 'Humidity',
+        pressure: 'Pressure',
+        wind_speed_avg: 'Wind Speed',
+        wind_heading: 'Wind Heading',
+        luminosity: 'Luminosity',
+      },
     }
   },
-  mounted(){
-    this.get_date();
+  mounted() {
+    this.get_date()
   },
   watch: {
     timestamp(newVal) {
-      const minute = new Date(newVal).getMinutes();
-      if (minute % 10 === 0){
-        console.log("time to fetch");
+      const minute = new Date(newVal).getMinutes()
+      if (minute % 10 === 0) {
+        console.log('time to fetch')
       }
     },
     dataHistory() {
       // if (this.sensorList.includes('lat-lon')) {
-      //   this.location = {'lon': this.dataHistory.data.lon, 'lat': this.dataHistory.data.lat};
+      // this.location = { lon: this.dataHistory.data[0].lon, lat: this.dataHistory.data[1].lat }
+      // console.log(this.dataHistory.data[0])
       // }
-      // this.sensorData = this.sensorList.filter(e => e !== 'lat-lon').map(sensor => {
-      //   return {
-      //     name: this.sensorName[sensor],
-      //     val: `${this.dataHistory.data[sensor]} ${this.dataHistory.unit[sensor]}`,
-      //     url: this.sensorIcon[sensor],
-      //   }
-      // });
-      console.log(this.sensorList);
-      console.log(this.timerange);
-      console.log(this.dataHistory);
-    }
+      const labels = Object.keys(this.dataHistory.data)
+
+      this.sensorData = this.sensorList
+        .filter((e) => e !== 'lat-lon')
+        .map((sensor) => {
+          return {
+            name: this.sensorName[sensor],
+            dates: labels,
+            unit: this.dataHistory.unit[sensor],
+            // val: labels.map((label) => this.dataHistory.data[label][sensor]),
+            url: useSensorIcons(sensor),
+          }
+        })
+    },
   },
   methods: {
     maj_sensor(sensorSelected) {
       if (sensorSelected.includes('all')) {
-        this.sensorList = Object.keys(this.sensorName);
-        this.sensorList.push('lat-lon');
-      }
-      else {
-        this.sensorList = sensorSelected.filter(e => e !== 'location' && e !== 'all');
-        this.location = {lon: 0, lat: 0};
+        this.sensorList = Object.keys(this.sensorName)
+        this.sensorList.push('lat-lon')
+      } else {
+        this.sensorList = sensorSelected.filter((e) => e !== 'location' && e !== 'all')
+        this.location = { lon: 0, lat: 0 }
       }
       if (sensorSelected.includes('location')) {
-        this.sensorList.push('lat-lon');
+        this.sensorList.push('lat-lon')
       }
+      this.fetchDataLive()
     },
 
-    maj_station(newStationName){
-      this.stationName = newStationName;
+    maj_station(newStationName) {
+      this.stationName = newStationName
     },
 
     maj_timeRange(newTimerange) {
-      this.timerange = newTimerange;
-      this.fetchDataLive();
+      this.timerange = newTimerange
+      this.fetchDataLive()
     },
 
     get_date() {
       setInterval(() => {
-        this.timestamp = new Date().toISOString();
-      }, 1_000);
+        this.timestamp = new Date().toISOString()
+      }, 1_000)
     },
 
-    // fetchDataLive() {
-    //   fetch("./live2.json")
-    //     .then(response => response.json())
-    //     .then(json => {
-    //       this.dataHistory=json;
-    //     });
-    // },
-    
-    async fetchDataLive() {
-      try {
-        const station = `http://piensg0${this.stationName.split(' ')[1]}.ensg.eu:3000/sample`;
-        const time = `${this.timerange.start}/${this.timerange.stop}`;
-        const sensor = this.sensorList.join('-');
-        const route = `${station}/${time}/${sensor}`;
+    fetchDataLive() {
+      fetch('./now.json')
+        .then((response) => response.json())
+        .then((json) => {
+          this.dataHistory = json
+        })
+    },
 
-        const response = await fetch(route);
-        if (response.ok) {
-          console.log(route)
-          this.dataHistory = await response.json();
-        } else {
-            throw new Error('Failed to fetch data');
-        }
-      } catch (error) {
-          console.error('Error:', error); 
-        }
-    }
+    // async fetchDataLive() {
+    //   try {
+    //     const station = `http://piensg0${this.stationName.split(' ')[1]}.ensg.eu:3000/sample`;
+    //     const time = `${this.timerange.start}/${this.timerange.stop}`;
+    //     const sensor = this.sensorList.join('-');
+    //     const route = `${station}/${time}/${sensor}`;
+
+    //     const response = await fetch(route);
+    //     if (response.ok) {
+    //       console.log(route)
+    //       this.dataHistory = await response.json();
+    //     } else {
+    //         throw new Error('Failed to fetch data');
+    //     }
+    //   } catch (error) {
+    //       console.error('Error:', error);
+    //     }
+    // }
   },
 }
-
 </script>
-
-
-  
--- dashboard/src/views/DataLiveView.vue
+++ dashboard/src/views/DataLiveView.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="container">
-      <div class="row mb-4">
-          <div class="col">
-              <HeaderApp @updateStationName="maj_station" />
-              <InfoStation :stationName="stationName" :timestamp="timestamp"/>
-          </div>
+    <div class="row mb-4">
+      <div class="col">
+        <HeaderApp @updateStationName="maj_station" />
+        <InfoStation :stationName="stationName" :timestamp="timestamp" />
       </div>
-      <div class="row">
-          <div class="col-2">
-              <MenuApp @updateSensor="maj_sensor" />
-          </div>
-          <div class="col-10">
-            <DataLiveBoard :sensorData="sensorData" :location="location"/>
-          </div>
+    </div>
+    <div class="row">
+      <div class="col-2">
+        <MenuApp @updateSensor="maj_sensor" />
       </div>
+      <div class="col-10">
+        <DataLiveBoard :sensorData="sensorData" :location="location" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -42,99 +42,94 @@ export default {
       sensorList: [],
       sensorData: [],
       dataLive: {},
-      timestamp: "",
-      location: {lon: 0, lat: 0},
-      stationName: "Pi 28",
-      sensorName: {"rain": "Precipitation", 
-                   "temperature": "Temperature", 
-                   "humidity": "Humidity",
-                   "pressure": "Pressure",
-                   "wind_speed_avg": "Wind Speed",
-                   "wind_heading": "Wind Heading",
-                   "luminosity": "Luminosity"
-                  },
-      sensorIcon: {"rain": PrecipIcon, 
-                   "temperature": TemperatureIcon, 
-                   "humidity": HumIcon,
-                   "pressure": PressIcon,
-                   "wind_speed_avg": SpeedIcon,
-                   "wind_heading": HeadIcon,
-                   "luminosity": LumIcon
-                  },
+      timestamp: '',
+      location: { lon: 0, lat: 0 },
+      stationName: 'Pi 28',
+      sensorName: {
+        rain: 'Precipitation',
+        temperature: 'Temperature',
+        humidity: 'Humidity',
+        pressure: 'Pressure',
+        wind_speed_avg: 'Wind Speed',
+        wind_heading: 'Wind Heading',
+        luminosity: 'Luminosity',
+      },
+      sensorIcon: {
+        rain: PrecipIcon,
+        temperature: TemperatureIcon,
+        humidity: HumIcon,
+        pressure: PressIcon,
+        wind_speed_avg: SpeedIcon,
+        wind_heading: HeadIcon,
+        luminosity: LumIcon,
+      },
     }
   },
-  mounted(){
-    // fetch("./live2.json")
-    //   .then(response => response.json())
-    //   .then(json => {
-    //     this.dataLive=json;
-    //     this.location = {'lon': json.data.long, 'lat': json.data.lat};
-    //   });
-
-    this.get_date();
+  mounted() {
+    this.get_date()
   },
   watch: {
     timestamp(newVal) {
-      const minute = new Date(newVal).getMinutes();
-      if (minute % 10 === 0){
-        console.log("time to fetch");
+      const minute = new Date(newVal).getMinutes()
+      if (minute % 10 === 0) {
+        console.log('time to fetch')
       }
     },
     dataLive() {
-
       if (this.sensorList.includes('lat-lon')) {
-        this.location = {'lon': this.dataLive.data.lon, 'lat': this.dataLive.data.lat};
+        this.location = { lon: this.dataLive.data.lon, lat: this.dataLive.data.lat }
       }
-      this.sensorData = this.sensorList.filter(e => e !== 'lat-lon').map(sensor => {
-        return {
-          name: this.sensorName[sensor],
-          val: `${this.dataLive.data[sensor]} ${this.dataLive.unit[sensor]}`,
-          url: this.sensorIcon[sensor],
-        }
-      });
-    }
+      this.sensorData = this.sensorList
+        .filter((e) => e !== 'lat-lon')
+        .map((sensor) => {
+          return {
+            name: this.sensorName[sensor],
+            val: `${this.dataLive.data[sensor]} ${this.dataLive.unit[sensor]}`,
+            url: this.sensorIcon[sensor],
+          }
+        })
+    },
   },
   methods: {
     maj_sensor(sensorSelected) {
       if (sensorSelected.includes('all')) {
-        this.sensorList = Object.keys(this.sensorName);
-        this.sensorList.push('lat-lon');
-      }
-      else {
-        this.sensorList = sensorSelected.filter(e => e !== 'location' && e !== 'all');
-        this.location = {lon: 0, lat: 0};
+        this.sensorList = Object.keys(this.sensorName)
+        this.sensorList.push('lat-lon')
+      } else {
+        this.sensorList = sensorSelected.filter((e) => e !== 'location' && e !== 'all')
+        this.location = { lon: 0, lat: 0 }
       }
       if (sensorSelected.includes('location')) {
-        this.sensorList.push('lat-lon');
+        this.sensorList.push('lat-lon')
       }
-      this.fetchDataLive();
+      this.fetchDataLive()
     },
 
-    maj_station(newStationName){
-      this.stationName = newStationName;
-      this.fetchDataLive();
+    maj_station(newStationName) {
+      this.stationName = newStationName
+      this.fetchDataLive()
     },
 
     get_date() {
       setInterval(() => {
-        this.timestamp = new Date().toISOString();
-      }, 1_000);
+        this.timestamp = new Date().toISOString()
+      }, 1_000)
     },
-    
+
     async fetchDataLive() {
       try {
-        const response = await fetch(`http://piensg0${this.stationName.split(' ')[1]}.ensg.eu:3000/live/${this.sensorList.join('-')}`);
+        const response = await fetch(
+          `http://piensg0${this.stationName.split(' ')[1]}.ensg.eu:3000/live/${this.sensorList.join('-')}`,
+        )
         if (response.ok) {
-            this.dataLive = await response.json();
+          this.dataLive = await response.json()
         } else {
-            throw new Error('Failed to fetch data');
+          throw new Error('Failed to fetch data')
         }
       } catch (error) {
-          console.error('Error:', error); 
-        }
-    }
+        console.error('Error:', error)
+      }
+    },
   },
 }
-
 </script>
-
